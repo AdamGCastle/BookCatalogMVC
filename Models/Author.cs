0639e353e66fdd531c2e7b8965a3ead799c5c9c5
AdamGCastle <88667052+AdamGCastle@users.noreply.github.com>
2021-11-15 09:21:38 +0200

    various improvements

---
+++ Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookCatalogMvc.Models
+{
+    public class Author
+    {        
+        public int ID { get; set; }
+        
+        [Required(ErrorMessage = "Please enter the Author's full name.")]
+        public string Name { get; set; }
+        public ICollection<Book> Books { get; set; }
+        
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookCatalogMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
